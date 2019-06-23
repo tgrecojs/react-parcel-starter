255fba3cf5f4a01122f4ec0dfdd204299cd35980
Thomas Greco <tgreco1610@gmail.com>
2019-06-23 03:16:11 -0400

    fixed issue with saga and removed default redux integration

--- source/App.js
+++ source/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import createStore from './store';
-import {Provider } from 'react-redux';
-const store = createStore;
 
 const App = ({title = 'Sample React App by TG'}) => (
-  <Provider store={store}>
+  <>
     <h2>{title}</h2>
-  </Provider>
+  </>
 );
 
 export default App;
